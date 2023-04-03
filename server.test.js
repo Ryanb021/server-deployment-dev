f5535166487bd3017fef5cc1d2df5b3c4821749a
Ryan Bagan <darcknight1980@gmail.com>
2023-04-03 14:31:21 -0700

    added files

---
+++ server.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { describe, default: test } = require('node:test');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+const supertest = require('supertest');
+
+const app = require('./server.js');
+const request = supertest(app);
+
+describe('As a user I want to send a request and get a capitalized message', () => {
+
+  test('Should return RYAN when sending a GET request to /capitalize-me', async () => {
+    let response = await request.get('/capitalize-me?message=Ryan');
+    expect(response.text).toEqual('RYAN');
+  })
+});
